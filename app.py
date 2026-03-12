2d3f76fe1f706235bba2e8767c5fc2e6aa16cb3e
cybercrime6q-dotcom <cybercrime6q@gmail.com>
2026-03-12 11:25:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ import time, sys, os, uuid
 app = Flask(__name__)
 db = None
 bucket = None
+import os
+import cloudinary
+
+cloudinary.config(
+  cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+  api_key=os.getenv("CLOUDINARY_API_KEY"),
+  api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
 
 # ============================
 # API KEYS CONFIGURATION
